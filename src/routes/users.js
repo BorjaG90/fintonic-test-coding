6b52f3af5c8c5da9ed7a7ffca453c1012c2080a9
Borja Gete <borjagete90@outlook.es>
2019-02-23 18:13:39 +0100

    Add User model

--- src/routes/users.js
+++ src/routes/users.js
@@ -8,4 +8,21 @@ router.get('/users/signup', (req, res) => {
   res.render('users/signup');
 });
 
+router.post('/users/signup', (req, res)=> {
+  const { name, email, pass, confirm_pass } = req.body;
+  const errors = [];
+  // Validation
+  if(pass != confirm_pass) {
+    errors.push({text: 'Las contraseñas no coinciden'});
+  }
+  if(pass.length < 6) {
+    errors.push({text: 'La contraseña debería ser mayor de 6 caracteres'});
+  }
+  if(errors.length > 0){
+    res.render('users/signup', {errors, name, email, pass, confirm_pass});
+  } else {
+    
+  }
+});
+
 module.exports = router;
