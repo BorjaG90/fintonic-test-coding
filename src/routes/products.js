6733a54659cec8c1ec42de495959788bfca1c1fe
Borja Gete <borjagete90@outlook.es>
2019-02-23 15:39:11 +0100

    Add form of new product

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,5 +1,14 @@
 const router = require('express').Router();
 
+router.get('/products/new', (req, res) => {
+  res.render('products/new_product');
+});
+
+router.post('/products/new', (req, res) => {
+  console.log(req.body);
+  res.send('OK');
+});
+
 router.get('/products', (req, res) =>{
   res.send('Lista de productos');
 });
