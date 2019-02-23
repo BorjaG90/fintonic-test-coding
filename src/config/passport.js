d35d5a2df6d517fe01bd996b53219479a923ffce
Borja Gete <borjagete90@outlook.es>
2019-02-23 18:44:04 +0100

    Add authentication settings

---
+++ src/config/passport.js
@@ -0,0 +1,31 @@
+const passport        = require('passport'),
+      LocalStrategy   = require('passport-local').Strategy;
+
+const User = require('../models/User');
+
+passport.use(new LocalStrategy({
+  usernameField: 'email'
+}, async (email, pasword, done) => {
+  const user =await User.findOne({email: email});
+  // Validation of user
+  if(!user) {
+    return done(null, false, {message: 'Usuario no encontrado.'});
+  } else {
+    const match = await User.matchPassword(pasword);
+    if(match) {
+      return done(null, user);
+    } else {
+      return done(null, false, {message: 'Contraseña incorrecta.'});
+    }
+  }
+}));
+
+passport.serializeUser((user, done) => {
+  return done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const User = require('../models/User')
 
+// Login
 router.get('/users/login', (req, res) => {
   res.render('users/login');
 });
