f04bd9ecc7f5e066ec57f4c3b2535409f67cd25d
Borja Gete <borjagete90@outlook.es>
2019-02-23 14:51:36 +0100

    Add Bootstrap4

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
 router.get('/users/login', (req, res) => {
-  res.send('Ingresando');
+  res.render('users/login');
 });
 
 router.get('/users/signup', (req, res) => {
-  res.send('Registro');
+  res.render('users/signup');
 });
 
 module.exports = router;
