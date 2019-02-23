470a13fb8cf34af3185497f78c6004208abb326e
Borja Gete <borjagete90@outlook.es>
2019-02-23 14:10:59 +0100

    Add view engine

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-const express = require('express');
-
+const   express     = require('express'),
+        path        = require('path'),
+        hbs         = require('express-handlebars');
 // Initializations
 const app = express();
 
 // Settings
 app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', hbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 // Middleware
 
