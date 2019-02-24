6e33ec76221cc3b5ea12d8e0a3a3632c737c506f
Borja Gete <borjagete90@outlook.es>
2019-02-24 16:07:33 +0100

    Add Footer

--- src/database.js
+++ src/database.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
+
 mongoose.connect(
+  // Uso una BBDD de mLab propia, 
+  // para usar una diferente, sustituir esta cadena de conexion 
   'mongodb://fintonic:fintonic201902@ds349045.mlab.com:49045/test-nodejs', {
     useCreateIndex: true,
     useNewUrlParser: true,
